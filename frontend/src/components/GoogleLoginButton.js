f5e2b785d68d20e0b24b385427933ca980c85c6a
chrisdoctor <chris.doctor@gmail.com>
2024-09-22 18:25:40 +0800

    Update login button component to remove fe login library to use be service

--- frontend/src/components/GoogleLoginButton.js
+++ frontend/src/components/GoogleLoginButton.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { useAuth } from '../context/AuthContext';
 
 const GoogleLoginButton = () => {
   const { login } = useAuth();
 
-  const handleLoginSuccess = (credentialResponse) => {
-    console.log(credentialResponse); 
-    login(credentialResponse); 
+  const handleGoogleLogin = async () => {
+    try {
+      // Redirect to the backend for Google OAuth login
+      window.location.href = `${process.env.REACT_APP_BACKEND_URL}/auth/google`;
+    } catch (error) {
+      console.error('Login error', error);
+    }
   };
 
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <GoogleLogin
-        onSuccess={handleLoginSuccess}
-        onError={(error) => {
-          console.log('Login Failed', error);
-        }}
-        scope="profile email openid"
-        // scope="https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid"
-      />
-    </GoogleOAuthProvider>
+    <button onClick={handleGoogleLogin}>
+      Login with Google
+    </button>
   );
 };
 
