36f3d1fd99369e1c9a84954859577a2203aad012
chrisdoctor <chris.doctor@gmail.com>
2024-09-22 17:52:22 +0800

    Initial setup of backend

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const passport = require('passport');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// const authRoutes = require('./routes/authRoutes');
+// const logRoutes = require('./routes/logRoutes');
+require('./config/passport'); // Load passport config
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+// Routes
+// app.use('/auth', authRoutes);
+// app.use('/api', logRoutes);
+
+module.exports = app;
