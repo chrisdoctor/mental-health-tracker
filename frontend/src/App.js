ded3012e8fa31211ba9c51e5145b1664977988aa
chrisdoctor <chris.doctor@gmail.com>
2024-09-22 20:00:35 +0800

    Integrate log components in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import './App.css';
 import { AuthProvider } from './context/AuthContext';
+import HomePage from './components/HomePage';
+import DailyLogForm from './components/DailyLogForm';
+import LogList from './components/LogList';
 
 const App = () => {
   return (
-      <AuthProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/logs" element={<LogList />} />
+          <Route path="/log" element={<DailyLogForm />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
