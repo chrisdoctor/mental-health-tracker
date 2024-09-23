ccc9c79fe3a37af74bd3ff45d1ca3b688fb0e1f3
chrisdoctor <chris.doctor@gmail.com>
2024-09-23 17:10:42 +0800

    Refactor middleware to use google id verification

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,19 +1,27 @@
-const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-const authenticateJWT = (req, res, next) => {
+const authenticateJWT = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
   if (!authHeader) return res.status(401).json({ message: 'Access denied, no token provided' });
 
-  const token = authHeader.split(' ')[1]; // Extract token from header
-
+  const token = authHeader.split(' ')[1]; // Extract the token from 'Bearer <token>'
   if (!token) return res.status(401).json({ message: 'Access denied, invalid token' });
 
   try {
-    const user = jwt.verify(token, JWT_SECRET); // Verify JWT token
-    req.user = user; // Attach the decoded user to the request object
-    next(); // Move to the next middleware/route handler
-  } catch (err) {
+    // Verify the token with Google OAuth2Client and public key
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,  // Must match the client ID
+    });
+
+    const payload = ticket.getPayload();  // Extract the user info from the token
+
+    // Attach the decoded user information to the request object
+    req.user = payload;
+    next(); // Proceed to the next middleware/route handler
+  } catch (error) {
+    console.error('Token verification error:', error.message);
     res.status(400).json({ message: 'Invalid token' });
   }
 };
