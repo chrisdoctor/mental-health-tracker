277dbc80ccb0925602d4a666a3de2d5b20b428ba
chrisdoctor <chris.doctor@gmail.com>
2024-09-23 12:04:55 +0800

    Check token in home page to ensure user is authenticated

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,13 +1,30 @@
-// src/components/HomePage.js
 import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import GoogleLoginButton from './GoogleLoginButton';
 
 const HomePage = () => {
+  const { token } = useAuth();
+  const navigate = useNavigate();
+
+  const handleAddLogClick = () => {
+    navigate('/log');  // Navigate to the daily log form
+  };
+
   return (
     <div>
-      <h1>Welcome to the Mental Health Progress Tracker</h1>
-      <p>To track your daily mental health progress, please login with Google.</p>
-      <GoogleLoginButton />
+      <h1>Welcome</h1>
+      {!token ? (
+        <div>
+          <p>Please log in using your Google account to start tracking your daily mental health progress.</p>
+          <GoogleLoginButton />
+        </div>
+      ) : (
+        <div>
+          <p>You are logged in. Start adding your daily log.</p>
+          <button onClick={handleAddLogClick}>Add Daily Log</button>
+        </div>
+      )}
     </div>
   );
 };
