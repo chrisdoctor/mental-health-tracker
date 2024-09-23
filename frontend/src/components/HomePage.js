26a007bceac73f7db317a5e6985d8c7c429bcc16
chrisdoctor <chris.doctor@gmail.com>
2024-09-23 18:42:12 +0800

    Add style to HomePage

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -32,21 +32,40 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome </h1>
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <div className="text-center p-8 bg-contrast shadow-md rounded-lg max-w-md">
+        <h1 className="text-4xl font-semibold text-primary mb-8">
+          Progress Tracker
+        </h1>
 
-      {token ? (
-        <div>
-          <p>You are logged in!</p>
-          <button onClick={handleLogout}>Logout</button>
+        {token ? (
           <div>
-            <button onClick={() => window.location.href = '/log'}>Add Daily Log</button>
-            <button onClick={() => window.location.href = '/logs'}>View Progress</button>
+            <p className="text-xl text-secondary mb-6">You are logged in!</p>
+            <button
+              onClick={handleLogout}
+              className="bg-primary hover:bg-accent text-white font-bold py-2 px-4 rounded mb-4"
+            >
+              Logout
+            </button>
+            <div>
+              <button
+                onClick={() => window.location.href = '/log'}
+                className="bg-secondary hover:bg-accent text-white font-bold py-2 px-6 rounded mb-4"
+              >
+                Add Daily Log
+              </button>
+              <button
+                onClick={() => window.location.href = '/progress'}
+                className="bg-accent hover:bg-secondary text-white font-bold py-2 px-6 rounded"
+              >
+                View Progress
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <GoogleLoginButton />  // Render login button if no token
-      )}
+        ) : (
+          <GoogleLoginButton />
+        )}
+      </div>
     </div>
   );
 };
