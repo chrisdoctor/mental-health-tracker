e5bd3664a3fb447767860d824c90e2907be27ef2
chrisdoctor <chris.doctor@gmail.com>
2024-09-24 16:20:02 +0800

    Backend app clean up

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const passport = require('passport');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const logRoutes = require('./routes/logRoutes');
-require('./config/passport'); // Load passport config
 
 dotenv.config();
 
@@ -13,7 +11,6 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(passport.initialize());
 
 // Routes
 app.use('/auth', authRoutes);
